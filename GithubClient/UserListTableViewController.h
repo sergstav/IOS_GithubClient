4799ea0cfb8ada62788789096ff58ef3b6c628d2
sergstav <serg.stavitckii@gmail.com>
2014-12-01 03:44:42 +0200

    App designed

--- GithubClient/UserListTableViewController.h
+++ GithubClient/UserListTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  UserListTableViewController.h
 //  GithubClient
 //
 //  Created by Sergey Stavitckii on 12/1/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface UserListTableViewController : UITableViewController
 
 @end
-
